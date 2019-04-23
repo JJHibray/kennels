e71c6c9ffaa40952f01f13aed5df3753d6b36885
Josh Hibray <jjhibray@gmail.com>
2019-04-22 22:17:21 -0500

    add functionality to animals component

--- src/components/ApplicationView.js
+++ src/components/ApplicationView.js
@@ -1,5 +1,6 @@
 import { Route } from 'react-router-dom'
 import React, { Component } from "react"
+import { withRouter } from 'react-router'
 import AnimalList from './animals/AnimalList'
 import LocationList from './locations/LocationList'
 import EmployeeList from './employees/EmployeeList'
@@ -8,10 +9,12 @@ import AnimalManager from "../modules/AnimalManager"
 import EmployeeManager from "../modules/EmployeeManager"
 import OwnerManager from "../modules/OwnerManager"
 import LocationManager from "../modules/LocationManager"
+import AnimalDetail from './animals/AnimalDetail'
+import AnimalForm from "./animals/AnimalForm"
 
 
 
-export default class ApplicationViews extends Component {
+ class ApplicationViews extends Component {
 
 
 
@@ -46,39 +49,47 @@ export default class ApplicationViews extends Component {
         })
     }
 
-    deleteAnimal = (id) => {
-        return AnimalManager.removeAndList(id)
-        .then(animals => this.setState({
-            animals: animals
-          })
-        )
-      }
+    deleteAnimal = (id) => AnimalManager.removeAndList(id)
+            .then (animals => {
+                this.props.history.push("/animals")
+                this.setState({ animals: animals })
+            })
 
     deleteEmployee = id => {
         return fetch(`http://localhost:5002/employees/${id}`, {
             method: "DELETE"
         })
-        .then(e => e.json())
-        .then(() => fetch(`http://localhost:5002/employees`))
-        .then(e => e.json())
-        .then(employees => this.setState({
-            employees: employees
-        })
-      )
+            .then(e => e.json())
+            .then(() => fetch(`http://localhost:5002/employees`))
+            .then(e => e.json())
+            .then(employees => this.setState({
+                employees: employees
+            })
+            )
     }
 
     deleteOwner = id => {
         return fetch(`http://localhost:5002/owners/${id}`, {
             method: "DELETE"
         })
-        .then(e => e.json())
-        .then(() => fetch(`http://localhost:5002/owners`))
-        .then(e => e.json())
-        .then(owners => this.setState({
-            owners: owners
-        })
-      )
-    }
+            .then(e => e.json())
+            .then(() => fetch(`http://localhost:5002/owners`))
+            .then(e => e.json())
+            .then(owners => this.setState({
+                owners: owners
+            })
+            )
+        }
+
+    addAnimal = animal =>
+    AnimalManager.post(animal)
+    .then(() => AnimalManager.getAll())
+    .then(animals =>
+      this.setState({
+        animals: animals
+      })
+    );
+
 
 
     render() {
@@ -87,20 +98,51 @@ export default class ApplicationViews extends Component {
                 <Route exact path="/" render={(props) => {
                     return <LocationList locations={this.state.locations} />
                 }} />
-                <Route exact path="/animals" render={() => {
-                    return <AnimalList deleteAnimal={this.deleteAnimal}
-                    animals={this.state.animals} />
+                <Route exact path="/animals" render={(props) => {
+                    return <AnimalList {...props}
+                    animals={this.state.animals}
+                    deleteAnimal={this.deleteAnimal}/>
+                }} />
+                {/*
+                // Our shiny new route. We pass employees to the AnimalForm so a dropdown can be populated */}
+                <Route path="/animals/new" render={(props) => {
+                    return <AnimalForm {...props}
+                       addAnimal={this.addAnimal}
+                       employees={this.state.employees} />
+                }} />
+
+                {/*
+                    This is a new route to handle a URL with the following pattern:
+                    http://localhost:3000/animals/1
+
+                    It will not handle the following URL because the `(\d+)`
+                    matches only numbers after the final slash in the URL
+                    http://localhost:3000/animals/jack
+                */}
+                <Route path="/animals/:animalId(\d+)" render={(props) => {
+                    // Find the animal with the id of the route parameter
+                    let animal = this.state.animals.find(animal =>
+                        animal.id === parseInt(props.match.params.animalId)
+                    )
+
+                    // If the animal wasn't found, create a default one
+                    if (!animal) {
+                        animal = { id: 404, name: "404", breed: "Dog not found" }
+                    }
+
+                    return <AnimalDetail animal={animal}
+                        deleteAnimal={this.deleteAnimal} />
                 }} />
                 <Route path="/employees" render={(props) => {
                     return <EmployeeList deleteEmployee={this.deleteEmployee}
-                    employees={this.state.employees} />
+                        employees={this.state.employees} />
                 }} />
                 <Route path="/owners" render={(props) => {
                     return <OwnerList deleteOwner={this.deleteOwner}
-                    owners={this.state.owners} />
+                        owners={this.state.owners} />
                 }} />
             </React.Fragment>
         )
     }
-}
+}   export default withRouter(ApplicationViews)
 
